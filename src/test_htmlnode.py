cca7518ff839f795631bcb1808deda50527754e5
Tom Clarke <thomashclarke25@outlook.com>
2025-05-15 12:58:02 +0100

    "Add LeafNode class and enhance HTMLNode functionality"

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,29 +1,69 @@
 import unittest
-from htmlnode import HTMLNode
-from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_eq(self):
-        node = HTMLNode("div", "This is a div", [TextNode("This is a text node", TextType.BOLD)], {"class": "container"})
-        node2 = HTMLNode("div", "This is a div", [TextNode("This is a text node", TextType.BOLD)], {"class": "container"})
-        self.assertEqual(node, node2)
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
 
-    def test_ne(self):
-        node = HTMLNode("div", "This is a div", [TextNode("This is a text node", TextType.BOLD)], {"class": "container"})
-        node2 = HTMLNode("div", "This is a div", [TextNode("This is a text node", TextType.BOLD)], {"class": "different"})
-        self.assertNotEqual(node, node2)
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
 
-    def test_ne_different_tag(self):
-        node = HTMLNode("div", "This is a div", [TextNode("This is a text node", TextType.BOLD)], {"class": "container"})
-        node2 = HTMLNode("span", "This is a div", [TextNode("This is a text node", TextType.BOLD)], {"class": "container"})
-        self.assertNotEqual(node, node2)
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
 
-    def test_ne_different_value(self):
-        node = HTMLNode("div", "This is a div", [TextNode("This is a text node", TextType.BOLD)], {"class": "container"})
-        node2 = HTMLNode("div", "This is a different div", [TextNode("This is a text node", TextType.BOLD)], {"class": "container"})
-        self.assertNotEqual(node, node2)
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
