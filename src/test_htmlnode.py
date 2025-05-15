4e7127b3538d7f58587052816b58cc99c2b96927
Tom Clarke <thomashclarke25@outlook.com>
2025-05-15 13:05:21 +0100

    Add ParentNode class with HTML generation and unit tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -49,6 +49,8 @@ class TestHTMLNode(unittest.TestCase):
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
 
+    # LeafNode tests
+
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
@@ -63,6 +65,59 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_no_tag(self):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
+
+    # ParentNode tests
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_parent_with_multiple_children(self):
+        child1 = LeafNode("p", "First child")
+        child2 = LeafNode("p", "Second child")
+        parent = ParentNode("div", [child1, child2])
+        self.assertEqual(
+            parent.to_html(),
+            "<div><p>First child</p><p>Second child</p></div>",
+        )
+    
+    def test_parent_with_no_children(self):
+        parent = ParentNode("div", [])
+        self.assertEqual(parent.to_html(), "<div></div>")
+    
+    def test_parent_with_props(self):
+        child = LeafNode("span", "Hello")
+        parent = ParentNode("div", [child], {"class": "container"})
+        self.assertEqual(
+            parent.to_html(),
+            '<div class="container"><span>Hello</span></div>',
+        )
+    
+    def test_complex_nesting(self):
+        leaf1 = LeafNode("b", "Bold text")
+        leaf2 = LeafNode("i", "Italic text")
+        inner_parent = ParentNode("p", [leaf1, leaf2], {"class": "text"})
+        leaf3 = LeafNode("a", "Link", {"href": "#"})
+        outer_parent = ParentNode("div", [inner_parent, leaf3], {"id": "content"})
+        self.assertEqual(
+            outer_parent.to_html(),
+            '<div id="content"><p class="text"><b>Bold text</b><i>Italic text</i></p><a href="#">Link</a></div>',
+        )
+    
+    def test_parent_with_none_children(self):
+        # Test handling of None instead of empty list
+        with self.assertRaises(ValueError):
+            ParentNode("div", None).to_html()
 
 
 if __name__ == "__main__":
