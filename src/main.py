68ca02690de5c448623b8f82297206e4b64a3175
Tom Clarke <thomashclarke25@outlook.com>
2025-05-15 21:02:07 +0100

    Add `split_nodes_delimiter` for inline markdown parsing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+from src.inline_markdown import split_nodes_delimiter
 from textnode import TextNode, TextType
 
 def main():
-    print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev").__repr__())
+    # print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev").__repr__())
+    node = TextNode("This is text with a `code block` word", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    print(new_nodes)
 
 
 if __name__ == "__main__":
