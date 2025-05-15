6daa60d5789f9beddf570109e651dec882ea474b
Tom Clarke <thomashclarke25@outlook.com>
2025-05-15 22:50:03 +0100

    Add text_to_textnodes function for parsing text into nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -2,6 +2,15 @@ import re
 
 from textnode import TextNode, TextType
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -86,5 +95,3 @@ def split_nodes_link(old_nodes):
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
-
-
