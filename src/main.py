1af2cf1c80c2328cb53f827183a78988f1b64c48
Tom Clarke <thomashclarke25@outlook.com>
2025-05-15 22:47:48 +0100

    Add support for splitting markdown images and links

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
-from src.inline_markdown import split_nodes_delimiter
+from src.inline_markdown import split_nodes_delimiter, split_nodes_image
 from textnode import TextNode, TextType
 
 def main():
     # print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev").__repr__())
-    node = TextNode("This is text with a `code block` word", TextType.TEXT)
-    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_image([node])
     print(new_nodes)
 
 
