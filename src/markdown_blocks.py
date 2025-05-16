503f071de628b61b51ded2a4eda18cc6d1337bab
Tom Clarke <thomashclarke25@outlook.com>
2025-05-16 09:52:08 +0100

    Refactor markdown_to_blocks to use list comprehension

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,9 +1,2 @@
 def markdown_to_blocks(markdown):
-    blocks = markdown.split("\n\n")
-    filtered_blocks = []
-    for block in blocks:
-        if block == "":
-            continue
-        block = block.strip()
-        filtered_blocks.append(block)
-    return filtered_blocks
+    return [blocks for blocks in markdown.split("\n\n") if blocks != ""]
