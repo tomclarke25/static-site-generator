b247b1ae538f936611348c2c20c2a0e514e36b0f
Tom Clarke <thomashclarke25@outlook.com>
2025-06-01 19:48:23 +0100

    Add blog pages and improve static site generation logic

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,5 +1,6 @@
 import os
 from markdown_blocks import markdown_to_html_node
+from pathlib import Path
 
 
 def generate_page(from_path, template_path, dest_path):
@@ -32,3 +33,13 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("no title found")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
